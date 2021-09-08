e8f5c2ef2faa633e6a7e321ae3643147868840c4
JusticeUA <vasya.ganec@gmail.com>
2021-09-08 22:43:07 +0300

    first commit

---
+++ #src/js/testWebP.js
@@ -0,0 +1,16 @@
+function testWebP(callback) {
+    let webP = new Image()
+    webP.onload = webP.onerror = function () {
+        callback(webP.height==2)
+    }
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    }
+})
+//
+let colors = {green, yellow, pink}
+colors[1]
+console.log(colors[1]);
